197597612cc3047daf6a3d0b87b65cf363f82f8c
deciphe <86434443+deciphe@users.noreply.github.com>
2026-09-16 13:33:04 -0400

    Ship payout-first site with Telegram access

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useEffect } from "react";
-import TrueRGuide from "./components/payoutlab/TrueRGuide";
 import Hero from "./components/payoutlab/Hero";
 import PayoutVault from "./components/payoutlab/PayoutVault";
-import FounderProof from "./components/payoutlab/FounderProof";
-import TrueRRankings from "./components/payoutlab/TrueRRankings";
-import ProprPick from "./components/payoutlab/ProprPick";
+import DirectAccess from "./components/payoutlab/DirectAccess";
 import SiteFooter from "./components/payoutlab/SiteFooter";
 import FloatingDock from "./components/payoutlab/FloatingDock";
 import { captureAttribution } from "./lib/analytics";
@@ -12,14 +9,11 @@ import { captureAttribution } from "./lib/analytics";
 export default function App() {
   useEffect(() => captureAttribution(), []);
 
-  if (new URLSearchParams(window.location.search).get("lesson") === "true-r") return <TrueRGuide />;
   return (
     <main className="relative min-h-screen w-full bg-void text-spectral selection:bg-lucid selection:text-void">
       <Hero />
       <PayoutVault />
-      <FounderProof />
-      <TrueRRankings />
-      <ProprPick />
+      <DirectAccess />
       <SiteFooter />
       <FloatingDock />
     </main>
